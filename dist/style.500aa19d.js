0155700c8771560d6862653c4b407b0629a670ff
joomiok <martlee21c@hanmail.net>
2020-11-21 15:07:55 +0900

    공지사항 디자인 완성

--- dist/style.500aa19d.js
+++ dist/style.500aa19d.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\icon-search.svg":[["icon-search.bf953e08.svg","images/icon-search.svg"],"images/icon-search.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\icon-search.svg":[["icon-search.bf953e08.svg","images/icon-search.svg"],"images/icon-search.svg"],"./..\\images\\icon-sound.svg":[["icon-sound.3e75ba1a.svg","images/icon-sound.svg"],"images/icon-sound.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "2862" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1596" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
