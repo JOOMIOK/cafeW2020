e2fd8857d300e52cc54c3aa69efa4d60f0bcffe7
joomiok <martlee21c@hanmail.net>
2020-11-22 11:43:34 +0900

    pic 섹션 디자인 완성

--- dist/style.500aa19d.js
+++ dist/style.500aa19d.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\images\\icon-search.svg":[["icon-search.bf953e08.svg","images/icon-search.svg"],"images/icon-search.svg"],"./..\\images\\icon-sound.svg":[["icon-sound.3e75ba1a.svg","images/icon-sound.svg"],"images/icon-sound.svg"],"./..\\images\\reserve-bg.jpg":[["reserve-bg.bf287b36.jpg","images/reserve-bg.jpg"],"images/reserve-bg.jpg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\images\\icon-search.svg":[["icon-search.bf953e08.svg","images/icon-search.svg"],"images/icon-search.svg"],"./..\\images\\icon-sound.svg":[["icon-sound.3e75ba1a.svg","images/icon-sound.svg"],"images/icon-sound.svg"],"./..\\images\\reserve-bg.jpg":[["reserve-bg.bf287b36.jpg","images/reserve-bg.jpg"],"images/reserve-bg.jpg"],"./..\\images\\image10.png":[["image10.3fc7f116.png","images/image10.png"],"images/image10.png"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3087" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1197" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
